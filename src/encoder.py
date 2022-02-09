9b14f426947c6a69a29cf1b558625db8c49da6d4
DK <dk.promsoft@mail.ru>
2022-02-09 17:45:25 +0700

    .

--- src/encoder.py
+++ src/encoder.py
@@ -3,7 +3,7 @@
 from typing import List
 
 
-def encode(s: str, preserve_case=True) -> List:
+def encode(s: str, preserve_case: bool = True) -> List:
     if not preserve_case:
         s = s.lower()
     return {x: i for i, x in enumerate({z: None for z in s.split()}.keys())}
